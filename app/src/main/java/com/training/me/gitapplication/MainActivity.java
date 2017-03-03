c90fb601c47ba69feeae25468d5d3b01579f7179
Fernando <fernandoestre@hotmail.com>
2017-03-03 13:07:18 -0500

    second step

--- app/src/main/java/com/training/me/gitapplication/MainActivity.java
+++ app/src/main/java/com/training/me/gitapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -15,6 +16,7 @@ public class MainActivity extends Activity {
         TextView v;
         Button b;
         EditText et;
+        Spinner sp;
 
 
 
